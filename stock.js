aeba78c91b6d9771fc99c51016678a5348d7a3c6
LegendAbdi <137934557+LegendAbdi@users.noreply.github.com>
2023-12-08 12:45:50 -0500

    Update stock.js

--- stock.js
+++ stock.js
@@ -3,8 +3,8 @@ const readline = require('readline');
 const fs = require('fs');
 const { MongoClient } = require('mongodb');
 
-const filePath = 'companies-1.csv'; // Change this to your CSV file path
-const connectionString = 'mongodb+srv://abdi:1@Abdi6057@companies.jyfn4ka.mongodb.net/Companies?retryWrites=true&w=majority';
+const filePath = 'companies.csv'; // Change this to your CSV file path
+const connectionString = 'mongodb+srv://abdi:<password>@companies.jyfn4ka.mongodb.net/Companies?retryWrites=true&w=majority';
 
 const dbName = 'Companies'; // Your database name
 const collectionName = 'companies'; // Your collection name
@@ -19,33 +19,48 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client
   const db = client.db(dbName);
   const collection = db.collection(collectionName);
 
-  var myFile = readline.createInterface({
-    input: fs.createReadStream(filePath)
+  const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+      // Read the CSV file and process data
+      var myFile = readline.createInterface({
+        input: fs.createReadStream(filePath)
+      });
+
+      myFile.on('line', function (line) {
+        const [companyName, stockTicker, stockPrice] = line.split(',');
+
+        // Create a new document to be inserted into the collection
+        const newData = {
+          companyName,
+          stockTicker,
+          stockPrice: parseFloat(stockPrice) // Assuming stockPrice is a number in the CSV
+        };
+
+        // Insert the new document into the collection
+        collection.insertOne(newData, function(err, result) {
+          if (err) {
+            console.error("Error inserting document:", err);
+          } else {
+            console.log("New document inserted:", newData);
+          }
+        });
+      });
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Data insertion process initiated.\n');
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found\n');
+    }
   });
 
-  myFile.on('line', function (line) {
-    const [companyName, stockTicker, stockPrice] = line.split(',');
+  const PORT = process.env.PORT || 3000; // Use the environment port or default to 3000
 
-    // Create a new document to be inserted into the collection
-    const newData = {
-      companyName,
-      stockTicker,
-      stockPrice: parseFloat(stockPrice) // Assuming stockPrice is a number in the CSV
-    };
-
-    // Insert the new document into the collection
-    collection.insertOne(newData, function(err, res) {
-      if (err) {
-        console.error("can't insert document", err);
-      } else {
-        console.log("Documented inserted", newData);
-      }
-    });
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
-
-  // Close the MongoDB connection after all insertions are done
-  client.close();
 });
+
 
 
 
